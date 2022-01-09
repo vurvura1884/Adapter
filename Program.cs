7fc37fa1a57f6edbe6abb4c69dde1e0c26909262
vurvura1884 <97406507+vurvura1884@users.noreply.github.com>
2022-01-09 23:07:44 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Program
+    {
+        class Cocktail
+        {
+            public void OrderAlcoholDrink()
+            {
+                Console.WriteLine("Ordering an alcoholic cocktail ");
+            }
+        }
+
+        public interface Drinks
+        {
+            void OrderDrink();
+        }
+
+        //Adapter
+        class Adapter : Drinks
+        {
+            private readonly Cocktail drink;
+            public Adapter(Cocktail alcoholFREEdrink)
+            {
+                this.drink = alcoholFREEdrink;
+            }
+            public void OrderDrink()
+            {
+                Console.WriteLine("Ordering a non-alcoholic cocktail ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Cocktail drink = new Cocktail();
+            drink.OrderAlcoholDrink();
+            Console.WriteLine("\n");
+            Drinks order = new Adapter(drink);
+            order.OrderDrink();
+        }
+    }
+}
